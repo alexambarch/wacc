a8b5c69f6fd0d40cd3df70725bcd3845f190edf3
Alex Ambarchian <alex@alexambarchian.com>
2025-07-12 20:14:52 -0400

    Initial commit, tokenizer for 1-1

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+use anyhow::{Result, bail};
+use clap::Parser;
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(PartialEq, Eq, Hash, Default, Clone)]
+struct Token {
+    ttype: TokenType,
+    value: String
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Default)]
+enum TokenType {
+    #[default]
+    Empty,
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    Semicolon,
+    Identifier,
+    Constant,
+    KWInt,
+    KWVoid,
+    KWReturn,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Run the lexer, stop before parsing
+    #[arg(long)]
+    lex: bool,
+
+    /// Run the lexer + parser, stop before assembler
+    #[arg(long)]
+    parse: bool,
+
+    /// Run the lexer + parser + assembler, stop before codegen
+    #[arg(long)]
+    codegen: bool,
+
+    /// The source file to compile
+    #[arg(value_name="file")]
+    filename: String
+}
+
+fn main() -> Result<()>{
+    let args = Args::parse();
+    let _early_exit = args.lex || args.parse || args.codegen;
+
+    let mut contents = read_file(args.filename)?;
+    let tokens = tokenize(&mut contents)?;
+
+    println!("Tokens: {}", tokens.len());
+
+    Ok(())
+}
+
+fn read_file(filename: String) -> Result<String> {
+    let mut file = File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+fn tokenize(contents: &mut String) -> Result<Vec<Token>> {
+    let mut regexps: HashMap<TokenType, Regex> = HashMap::new();
+    regexps.insert(TokenType::OpenParen, Regex::new(r"\(").unwrap());
+    regexps.insert(TokenType::CloseParen, Regex::new(r"\)").unwrap());
+    regexps.insert(TokenType::OpenBrace, Regex::new(r"{").unwrap());
+    regexps.insert(TokenType::CloseBrace, Regex::new(r"}").unwrap());
+    regexps.insert(TokenType::Semicolon, Regex::new(r";").unwrap());
+    regexps.insert(TokenType::KWReturn, Regex::new(r"return").unwrap());
+    regexps.insert(TokenType::KWVoid, Regex::new(r"void").unwrap());
+    regexps.insert(TokenType::KWInt, Regex::new(r"int").unwrap());
+    regexps.insert(TokenType::Constant, Regex::new(r"([0-9]+)\b").unwrap());
+    regexps.insert(TokenType::Identifier, Regex::new(r"([a-zA-Z_]\w*)\b").unwrap());
+
+    let mut tokens: Vec<Token> = Vec::new();
+    while !contents.is_empty() {
+        *contents = contents.trim_start().to_owned();
+
+        let mut longest = 0;
+        let mut longest_token = Token::default();
+        for (token, regexp) in &regexps {
+            if let Some(s) = regexp.find(contents)  && s.len() > longest {
+                longest = s.len();
+                // If we matched a constant or ident, fill in the value with the capture, otherwise continue
+                match *token {
+                    TokenType::Constant | TokenType::Identifier => {
+                        let cap = regexp.captures(contents).unwrap().get(0).unwrap();
+                        longest_token = Token{ttype: token.clone(), value: cap.as_str().to_owned()};
+
+                    }
+
+                    _ => {
+                        longest_token = Token{ttype: token.clone(), value: "".to_owned()};
+                    }
+                }
+            }
+        }
+
+        if longest == 0 {
+            bail!("Unable to tokenize :(");
+        }
+
+        tokens.push(longest_token.clone());
+        *contents = contents.split_off(longest);
+    }
+
+    Ok(vec![])
+}
